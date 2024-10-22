46882ac8b8b30f6d609175eb1d2d8be8912d8e20
sammypanda <38725335+sammypanda@users.noreply.github.com>
2024-10-22 19:31:32 +1000

    add basic layout+content

--- _config.ts
+++ _config.ts
@@ -4,7 +4,9 @@ import robots from "lume/plugins/robots.ts";
 import sass from "lume/plugins/sass.ts";
 import sitemap from "lume/plugins/sitemap.ts";
 
-const site = lume();
+const site = lume({
+    src: "./src" // set the website source directory
+});
 
 site.use(metas());
 site.use(robots());
