45ffed67a75b7dd1569732b174727f48224822e2
sammypanda <38725335+sammypanda@users.noreply.github.com>
2024-11-01 09:17:20 +1000

    improve inline svg/img implementation (inline plugin)

--- _config.ts
+++ _config.ts
@@ -3,6 +3,7 @@ import metas from "lume/plugins/metas.ts";
 import robots from "lume/plugins/robots.ts";
 import sass from "lume/plugins/sass.ts";
 import sitemap from "lume/plugins/sitemap.ts";
+import inline from "lume/plugins/inline.ts";
 
 const site = lume({
     src: "./src" // set the website source directory
@@ -12,6 +13,8 @@ site.use(metas());
 site.use(robots());
 site.use(sass());
 site.use(sitemap());
+// the convenience of img svgs with the functionality of in-line svgs
+site.use(inline());
 
 site.copy("img");
 
